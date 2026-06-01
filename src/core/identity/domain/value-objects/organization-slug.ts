5acfe822365b7add8a68cbba92f9a0a1b3d7c148
Luciano Douglas Machado Chagas <olucianochagas@gmail.com>
2026-06-01 11:04:18 -0300

    feat(identity): OrganizationSlug value object derivado de texto

---
+++ src/core/identity/domain/value-objects/organization-slug.ts
@@ -0,0 +1,21 @@
+import { ValueObject } from '@/core/shared/domain/value-object'
+
+export class OrganizationSlug extends ValueObject<{ value: string }> {
+  private constructor(value: string) {
+    super({ value })
+  }
+
+  get value(): string {
+    return this.props.value
+  }
+
+  static fromText(text: string): OrganizationSlug {
+    const slug = text
+      .normalize('NFKD')
+      .replace(/[̀-ͯ]/g, '') // remove acentos
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/^-+|-+$/g, '')
+    return new OrganizationSlug(slug.length > 0 ? slug : 'org')
+  }
+}
