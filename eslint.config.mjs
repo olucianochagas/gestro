62f1b857e4e0b7b3879da50f3ec11db712cd87eb
Luciano Douglas Machado Chagas <olucianochagas@gmail.com>
2026-06-02 09:56:48 -0300

    chore: eslint ignora .remember/ e normaliza cypress/tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,8 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Tooling-managed scratch dir (not project source):
+    ".remember/**",
   ]),
 ]);
 
